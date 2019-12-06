66ce28a59da058d8c933c286cbe42e811d68ed35
Zoe Judd <dog456pop@gmail.com>
2019-12-06 11:13:36 -0800

    Remove rodux

--- src/Shared/Actions/Interface/SetLanguage.lua
+++
@@ -1,9 +0,0 @@
-local import = require(game.ReplicatedStorage.Lib.Import)
-
-local Action = import "Action"
-
-return Action(script.Name, function(language)
-	return {
-		language = language
-	}
-end)
--- src/Shared/Actions/Interface/SetLayout.lua
+++
@@ -1,9 +0,0 @@
-local import = require(game.ReplicatedStorage.Lib.Import)
-
-local Action = import "Action"
-
-return Action(script.Name, function(layoutType)
-	return {
-		layoutType = layoutType
-	}
-end)
