acf318d081fefd20a018ee4899b15c9b6edd5fb5
unknown <dog456pop@gmail.com>
2019-12-21 17:05:04 -0700

    Change command button to +

--- src/Client/Systems/ClientCommands.lua
+++ src/Client/Systems/ClientCommands.lua
@@ -5,7 +5,7 @@ local Commands = {}
 function Commands.start()
 	local Cmdr = require(game.ReplicatedStorage:WaitForChild("CmdrClient"))
 	-- Configurable, and you can choose multiple keys
-	Cmdr:SetActivationKeys({ Enum.KeyCode.Tilde, Enum.KeyCode.Semicolon })
+	Cmdr:SetActivationKeys({Enum.KeyCode.Equals})
 end
 
 return Commands
